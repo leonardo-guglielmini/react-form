70c96d64b6531d173de7312c6a29414d99f0a103
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-02 15:55:24 +0100

    ADD main.jsx | new post form content, tags

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,13 +3,14 @@ import Card from "./ui/Card/card"
 import basePosts from "../data/posts"
 
 import { useState } from 'react'
-const tags = []
 
 export default function Main(){
 
-
+    let tags=[]
     const [posts, setPosts] = useState(basePosts)
     const [newPostTitle, setNewPostTitle] = useState("")
+    const [newPostContent, setNewPostContent] = useState("")
+    const [newPostTags, setNewPostTags] = useState("")
 
     function onSubmit(event){
         event.preventDefault()
@@ -20,8 +21,8 @@ export default function Main(){
             id: posts.at(-1).id+1,
             title: newPostTitle,
             image: undefined,
-            content: "",
-            tags:[],
+            content: newPostContent,
+            tags:[...newPostTags.split(" ")],
             published:true
         }
 
@@ -36,12 +37,13 @@ export default function Main(){
         setPosts(posts.filter(post=> post.id!=id))
     }
 
-
     return(
         <main className={style.mainContent}>
             <div className="container">
                 <form onSubmit={onSubmit} action="">
                         <input  onChange={(e)=>setNewPostTitle(e.target.value)} type="text" placeholder="Titolo del post" value={newPostTitle}/>
+                        <input  onChange={(e)=>setNewPostContent(e.target.value)} type="text" placeholder="Descrizione del post" value={newPostContent}/>
+                        <input  onChange={(e)=>setNewPostTags(e.target.value)} type="text" placeholder="Tags del post" value={newPostTags}/>
                         <input type="submit" value="Invia"/>
                 </form>
 
@@ -49,8 +51,8 @@ export default function Main(){
                     {posts.map((post)=>
                         post.published===true ?
                         <div className={style.col3} key={post.id}>
-                            <Card callback={()=>deletePost(post.id)} title={post.title} image={post.image} content={post.content} tags={post.tags} published={post.    published}/>
-                            {post.tags.forEach(tag => !tags.find((tagEl) => tagEl === tag) ? tags.push(tag) : null)}
+                            <Card callback={()=>deletePost(post.id)} title={post.title} image={post.image} content={post.content} tags={post.tags} published={post.published}/>
+                            {post.tags.forEach(tag => !tags.find((el) => el === tag) ? tags.push(tag) : null)}
                         </div> : null
                     )}
                 </section>
